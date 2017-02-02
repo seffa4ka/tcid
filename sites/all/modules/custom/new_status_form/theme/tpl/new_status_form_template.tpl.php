2519faaa033a1e8a015f308c17d7f52fb923383c
seffka <th-seffka@mail.ru>
2017-02-02 16:44:20 +0600

    added css

--- sites/all/modules/custom/new_status_form/theme/tpl/new_status_form_template.tpl.php
+++ sites/all/modules/custom/new_status_form/theme/tpl/new_status_form_template.tpl.php
@@ -8,22 +8,56 @@
  */
 ?>
 <div class="custom-template-wrapper">
-  <div class="new-body">
-    <?php
-    foreach ($variables['body'] as $title => $messages) {
-    ?>
-      <div class="new-title">
-        <?php echo $title; ?>
-      </div>
-      <div class="new-messages">
+  <div class="new-mask"></div>
+  <div class="new-window">
+    <div class="new-title">
+      <div class="new-title-message">New status form</div>
+      <button class="new-title-button">X</button>
+    </div>
+    <div class="new-body">
+      <?php
+      foreach ($variables['body'] as $title => $messages) {
+      ?>
         <?php
-        foreach ($messages as $message) {
-          echo $message . '<br>';
+        if($title === 'status'){
+        ?>
+        <div class="new-status-messages">
+          <?php
+          foreach ($messages as $message) {
+            echo $message . '<br>';
+          }
+          ?>
+        </div>
+        <?php
+        }
+
+        if($title === 'error'){
+        ?>
+        <div class="new-error-messages">
+          <?php
+          foreach ($messages as $message) {
+            echo $message . '<br>';
+          }
+          ?>
+        </div>
+        <?php
+        }
+
+        if($title === 'warning'){
+        ?>
+        <div class="new-warning-messages">
+          <?php
+          foreach ($messages as $message) {
+            echo $message . '<br>';
+          }
+          ?>
+        </div>
+        <?php
         }
         ?>
-      </div>
-    <?php
-    }
-    ?>
+      <?php
+      }
+      ?>
+    </div>
   </div>
 </div>
