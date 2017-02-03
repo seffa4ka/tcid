e31e073d6a123ec7ff4291f35b620abf406d65cc
seffka <th-seffka@mail.ru>
2017-02-03 16:39:03 +0600

    fix js

--- sites/all/modules/custom/new_status_form/theme/js/new_status_form_js.js
+++ sites/all/modules/custom/new_status_form/theme/js/new_status_form_js.js
@@ -1,8 +1,22 @@
 (function($) {
-Drupal.behaviors.myBehavior = {
+Drupal.behaviors.new_status = {
   attach: function (context) {
     $(document).ready(function(){
-      $(".new-window").offset({top: $(".new-mask").outerHeight()/2 - 100, left: $(".new-mask").outerWidth()/2 - 250});
+        
+      if (Drupal.settings.new_status.form_heidth !== null && Drupal.settings.new_status.form_width !== null){
+        var form_h = Drupal.settings.new_status.form_heidth;
+        var form_w = Drupal.settings.new_status.form_width;
+      }
+      else {
+        var form_h = 200;
+        var form_w = 500;
+      }
+      
+      $(".new-window").height(form_h);
+      
+      $(".new-window").width(form_w);
+      
+      $(".new-window").offset({top: $(".new-mask").outerHeight()/2 - form_h/2, left: $(".new-mask").outerWidth()/2 - form_w/2});
 
       $('.new-title-button').click(function(){
         $('.custom-template-wrapper').hide();
