34e520598292b25b9d8328fd60630a39ff7f68b3
seffka <th-seffka@mail.ru>
2017-02-03 17:07:50 +0600

    added settings

--- sites/all/modules/custom/new_status_form/theme/js/new_status_form_js.js
+++ sites/all/modules/custom/new_status_form/theme/js/new_status_form_js.js
@@ -3,18 +3,32 @@ Drupal.behaviors.new_status = {
   attach: function (context) {
     $(document).ready(function(){
         
-      if (Drupal.settings.new_status.form_heidth !== null && Drupal.settings.new_status.form_width !== null){
-        var form_h = Drupal.settings.new_status.form_heidth;
-        var form_w = Drupal.settings.new_status.form_width;
+      if (Drupal.settings.new_status.form_height !== null) {
+        var form_h = Drupal.settings.new_status.form_height; 
       }
       else {
         var form_h = 200;
+      }
+      
+      if(Drupal.settings.new_status.form_width !== null){
+        var form_w = Drupal.settings.new_status.form_width;
+      }
+      else {
         var form_w = 500;
+      }
+      
+      if(Drupal.settings.new_status.form_bg !== null){
+        var form_bg = Drupal.settings.new_status.form_bg;
+      }
+      else {
+        var form_bg = "burlywood";
       }
       
       $(".new-window").height(form_h);
       
       $(".new-window").width(form_w);
+      
+      $(".new-body").css("background-color", form_bg);
       
       $(".new-window").offset({top: $(".new-mask").outerHeight()/2 - form_h/2, left: $(".new-mask").outerWidth()/2 - form_w/2});
 
